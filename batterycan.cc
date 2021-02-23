4cb03a0dbad2eb9fa73ee096f00a9d1690056df7
Ian Ulanday <ulanday.ian@gmail.com>
2021-02-23 16:27:01 -0700

    added printraw and readAllCellVoltage functions

--- batterycan.cc
+++ batterycan.cc
@@ -1,2 +1,23 @@
 #include "batterycan.hh"
 
+void printRaw(CAN_message_t msg) {
+    Serial.print(msg.id, HEX); Serial.print(" ");
+    for (int i = 0; i < 8; i++) {
+      Serial.print(msg.buf[i], HEX); Serial.print(" ");
+    }
+    Serial.println("");
+}
+
+void readAllCellVoltage(uint8_t packId, CAN_message_t *msg, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16> *can) {
+    msg->flags.extended = true;
+    msg->id = 0x1000510F | (packId << 4);
+    msg->buf[0] = 0x01;
+    msg->buf[1] = 0xF5;
+    msg->buf[2] = 0x23;
+    msg->buf[3] = 0x01;
+    msg->buf[4] = 0x00;
+
+    can->write(*msg);
+    bool first = true;
+    int counter = 1;
+}
--- batterycan.hh
+++ batterycan.hh
@@ -4,4 +4,7 @@
 #include <FlexCAN_T4.h>
 #include <math.h>
 
+#define REQ_ALL_CELL_VOLTAGES 0x1F4
 
+void printRaw(CAN_message_t msg);
+void readAllCellVoltage(uint8_t packId, CAN_message_t *msg, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16> *can);
