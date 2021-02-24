52fe08982705774f5393f881e5eb505e607b8b5b
Ian Ulanday <ulanday.ian@gmail.com>
2021-02-24 13:47:08 -0700

    changed thangs

--- batterycan.cc
+++ batterycan.cc
@@ -1,6 +1,8 @@
 #include "batterycan.hh"
 
-void printRaw(CAN_message_t msg) {
+CAN_message_t battery_message;
+
+void printRawBat(CAN_message_t msg) {
     Serial.print(msg.id, HEX); Serial.print(" ");
     for (int i = 0; i < 8; i++) {
       Serial.print(msg.buf[i], HEX); Serial.print(" ");
@@ -8,16 +10,14 @@ void printRaw(CAN_message_t msg) {
     Serial.println("");
 }
 
-void readAllCellVoltage(uint8_t packId, CAN_message_t *msg, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16> *can) {
-    msg->flags.extended = true;
-    msg->id = 0x1000510F | (packId << 4);
-    msg->buf[0] = 0x01;
-    msg->buf[1] = 0xF5;
-    msg->buf[2] = 0x23;
-    msg->buf[3] = 0x01;
-    msg->buf[4] = 0x00;
+void readAllCellVoltage(uint8_t packId, CAN_message_t *battery_message, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16> *can) {
+    battery_message->flags.extended = true;
+    battery_message->id = 0x1000510F | (packId << 4);
+    battery_message->buf[0] = 0x01;
+    battery_message->buf[1] = 0xF5;
+    battery_message->buf[2] = 0x23;
+    battery_message->buf[3] = 0x01;
+    battery_message->buf[4] = 0x00;
 
-    can->write(*msg);
-    bool first = true;
-    int counter = 1;
+    can->write(*battery_message);
 }
