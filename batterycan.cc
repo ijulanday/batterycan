a9856e6c3996e3fe4e9f5e147d8cd90bb296adb2
Ian Ulanday <ulanday.ian@gmail.com>
2021-02-26 08:58:11 -0700

    revised cell voltage command to include message length (necessary) and deleted message arg to use shared message variable instead

--- batterycan.cc
+++ batterycan.cc
@@ -10,14 +10,14 @@ void printRawBat(CAN_message_t msg) {
     Serial.println("");
 }
 
-void readAllCellVoltage(uint8_t packId, CAN_message_t *battery_message, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16> *can) {
-    battery_message->flags.extended = true;
-    battery_message->id = 0x1000510F | (packId << 4);
-    battery_message->buf[0] = 0x01;
-    battery_message->buf[1] = 0xF5;
-    battery_message->buf[2] = 0x23;
-    battery_message->buf[3] = 0x01;
-    battery_message->buf[4] = 0x00;
-
-    can->write(*battery_message);
+void readAllCellVoltage(uint8_t packId, FlexCAN_T4<CAN1, RX_SIZE_256, TX_SIZE_16> *can) {
+    battery_message.flags.extended = true;
+    battery_message.id = 0x1000510F | (packId << 4);
+    battery_message.buf[0] = 0x01;
+    battery_message.buf[1] = 0xF5;
+    battery_message.buf[2] = 0x23;
+    battery_message.buf[3] = 0x01;
+    battery_message.buf[4] = 0x00;
+    battery_message.len = 5;
+    can->write(battery_message);
 }
